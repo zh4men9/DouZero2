18a6f0665c8c66e6c709fdb16b735c2b78779d14
zh4men9 <zh4men9@163.com>
2023-05-20 10:30:30 +0800

    picture 1

--- plot_picture.py
+++ plot_picture.py
@@ -13,4 +13,33 @@
 # LandlordUP 蓝色
 
 # 从./douzero_checkpoints/douzero/logs.csv读取数据
-# logs.csv中的数据格式如下：
+# logs.csv中的数据有多列，其中frames代表timesteps, loss_landlord, loss_landlord_up, loss_landlord_down为对应的损失
+# 将图片按照如下格式保存./image/loss/loss_{frames}_landlord_down_up.png
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 读取CSV文件中的数据
+logs = pd.read_csv('./douzero_checkpoints/douzero/logs.csv')
+
+# 按照timesteps（frames）排序
+logs = logs.sort_values(by=['frames'])
+
+# 绘制Landlord、LandlordDown和LandlordUP的损失曲线
+plt.plot(logs['frames'], logs['loss_landlord'], color='red', label='Landlord')
+plt.plot(logs['frames'], logs['loss_landlord_down'], color='green', label='LandlordDown')
+plt.plot(logs['frames'], logs['loss_landlord_up'], color='blue', label='LandlordUP')
+
+# 设置标题和轴标签
+plt.title('Loss Curve')
+plt.xlabel('Timesteps')
+plt.ylabel('Loss')
+
+# 显示图例
+plt.legend()
+
+# 保存图片到本地路径./image/loss/
+filename = './image/loss/loss_{}_landlord_down_up.png'.format(int(logs['frames'].max()))
+plt.savefig(filename)
+
+# 显示图表
+plt.show()
